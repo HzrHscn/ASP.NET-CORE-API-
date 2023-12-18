d954c1c861be2d5dfed35e5efa69359a074fb4a5
Hazar Hascan <96652029+HzrHscn@users.noreply.github.com>
2023-12-18 21:36:22 +0300

    Hazar Hascan

---
+++ BusinessLayer/Concrete/AssignmentManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AssignmentManager : IAssignmentService
+    {
+        private readonly IAssignmentDal _assignmentDal;
+
+        public AssignmentManager(IAssignmentDal assignmentDal)
+        {
+            _assignmentDal = assignmentDal;
+        }
+
+        public void TDelete(Assignment t)
+        {
+            _assignmentDal.Delete(t);
+        }
+
+        public Assignment TGetById(int id)
+        {
+            return _assignmentDal.GetById(id);
+        }
+
+        public List<Assignment> TGetList()
+        {
+            return _assignmentDal.GetList();
+        }
+
+        public void TInsert(Assignment t)
+        {
+            _assignmentDal.Insert(t);
+        }
+
+        public void TUpdate(Assignment t)
+        {
+            _assignmentDal.Update(t);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/RepairRequestManager.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class RepairRequestManager : IRepairRequestService
+    {
+        private readonly IRepairRequestDal _repairRequestDal;
+        public void TDelete(RepairRequest t)
+        {
+            _repairRequestDal.Delete(t);
+        }
+
+        public RepairRequest TGetById(int id)
+        {
+            return _repairRequestDal.GetById(id);
+        }
+
+        public List<RepairRequest> TGetList()
+        {
+            return _repairRequestDal.GetList();
+        }
+
+        public void TInsert(RepairRequest t)
+        {
+            _repairRequestDal.Insert(t);
+        }
+
+        public void TUpdate(RepairRequest t)
+        {
+            _repairRequestDal.Update(t);
+        }
+    }
+}
